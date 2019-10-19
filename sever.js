0453d712fd9fcf0f5f633f833c2af8ad58ef6dcf
FaruqueBraimo <faruquebraimo@gmail.com>
2019-10-19 11:46:40 -0700

    j
    j

--- sever.js
+++ sever.js
@@ -1,10 +1,7 @@
-const
-  express = require('express'),
-  serveStatic = require('serve-static'),
-  history = require('connect-history-api-fallback'),
-  port = process.env.PORT || 5000
-
-const app = express()
-app.use(history())
-app.use(serveStatic(__dirname + '/dist/spa'))
-app.listen(port)
+const express = require('express');
+const serveStatic = require("serve-static")
+const path = require('path');
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 80;
+app.listen(port);
